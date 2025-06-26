e474521a1dcc9a401663d84af33fea685fd27c5d
yober cieza <gctic.sgsass12@essalud.gob.pe>
2025-06-25 23:41:56 -0500

    Añadiendo servicio de correo y pass clase 3

--- src/app/modules/auth/components/login-form/login-form.component.ts
+++ src/app/modules/auth/components/login-form/login-form.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { faPen, faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import { AuthService } from '@services/auth.service';
+import {RequestStatus} from '@models/estados.model'
+
 
 @Component({
   selector: 'app-login-form',
@@ -17,17 +20,28 @@ export class LoginFormComponent {
   faEye = faEye;
   faEyeSlash = faEyeSlash;
   showPassword = false;
-  status: string = 'init';
+  status:RequestStatus='init';
 
   constructor(
     private formBuilder: FormBuilder,
-    private router: Router
+    private router: Router,
+    private authService:AuthService,
+
   ) { }
 
   doLogin() {
     if (this.form.valid) {
       this.status = 'loading';
       const { email, password } = this.form.getRawValue();
+      this.authService.login(email,password).subscribe({
+        next:()=>{
+          this.status='success'
+          this.router.navigate(['/app'])
+
+        },error:()=>{
+          this.status='failed'
+        }
+      })
       // TODO
     } else {
       this.form.markAllAsTouched();
